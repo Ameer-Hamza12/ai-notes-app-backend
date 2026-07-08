5e69b5ccb955cc5212b3717924cf6f6f1a82f4d8
devlogixloop <dev.logixloop@gmail.com>
2026-07-08 15:41:53 +0500

    configure express application with security middleware and error handling

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import errorHandler from "./middlewares/errorHandler.js";
+
+import env from "./config/env.js";
+
+const app = express();
+app.use(errorHandler);
+
+app.use(
+  cors({
+    origin: env.clientUrl,
+    credentials: true,
+  }),
+);
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+app.use(helmet());
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.get("/api/v1/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is running successfully",
+  });
+});
+
+export default app;
---
+++ src/config/env.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const env = {
+  port: process.env.PORT || 5000,
+  nodeEnv: process.env.NODE_ENV || "development",
+  clientUrl: process.env.CLIENT_URL,
+  mongoUri: process.env.MONGODB_URI,
+  jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
+  jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
+};
+
+export default env;
